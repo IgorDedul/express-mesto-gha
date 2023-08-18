d0eefe4f63064004f52f44fd13fd15906fec46dd
IgorDedul <dedul_igor@mail.ru>
2023-08-19 02:20:08 +0300

    feat: add email and password in user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+//const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,26 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://i.pinimg.com/736x/3f/0e/65/3f0e65097bd1c16017156c2a1b4b4ba1--jacques-cousteau-costume-ideas.jpg',
+    validate: {
+      validator: (string) => {
+        validator.isURL(string);
+      },
+    },
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: (string) => {
+        validator.isEmail(string);
+      },
+    },
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
